c4be067f922d01b10b7895bd81001e493b31e480
est5 <ghostrekon5500@ya.ru>
2022-11-21 20:39:52 +0300

    add onCreate

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -30,7 +30,11 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-
+                    ArticleWithImg(
+                        title = stringResource(id = R.string.article_title),
+                        firstParagraph = stringResource(id = R.string.article_first_paragraph),
+                        secondParagraph = stringResource(id = R.string.article_second_paragraph),
+                    )
                 }
             }
         }
